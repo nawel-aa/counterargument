cdb22899f25289ca905bfa9a2b739866ed9b2024
Nawel Aammar <nawel.aa@hotmail.fr>
2020-11-23 15:22:34 -0500

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/search', to: 'pages#search'
+  
+  resources :arguments, except: %i[destroy index] do
+    resources :votes, only: :create
+  end
+
+
+  get '/profile', to: 'profiles#show'
+  get '/profile/edit', to: 'profiles#edit', as: "user"
+  patch '/profile/:id', to: 'profiles#update'
 end
