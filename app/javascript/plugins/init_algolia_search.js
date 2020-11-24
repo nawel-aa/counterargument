f0e2abbd0aaaaa553231808c69b0d96b00ef5c7b
Nooshin Attarian <noshinatarian@gmail.com>
2020-11-24 18:03:36 -0500

    saving everything

--- app/javascript/plugins/init_algolia_search.js
+++ app/javascript/plugins/init_algolia_search.js
@@ -14,8 +14,8 @@ const algoliaSearch = () => {
     inputField.addEventListener("input", () => {
       index.search(inputField.value).then((content) => {
 
-        // rab the li - queryselect the ul
-        // parsing the json to create the displayed html
+        // const list = document.querySelector('#text');
+        // parsing the json to create the displayed html ---> display = JSON.parse(#text)
         // assigning of inner html to step 2
 
         console.log(content);
@@ -25,5 +25,4 @@ const algoliaSearch = () => {
     });
   }
 }
-
-export { algoliaSearch };
+export {algoliaSearch};
