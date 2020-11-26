433c4c673a824ee3f056a9574453b4aa90fb922f
Nawel Aammar <nawel.aa@hotmail.fr>
2020-11-26 11:29:01 -0500

    On track to make the search work.

--- app/models/argument.rb
+++ app/models/argument.rb
@@ -1,31 +1,28 @@
 class Argument < ApplicationRecord
   # this will create an Argument index and add search capabilities to the Argument model:
   include AlgoliaSearch
-
+  
   algoliasearch do
     # list of attribute used to build an Algolia record
     attributes :content
-      attribute :user do
-        { name: user.nickname, id: user.id, email: user.email }
-      end
   end
-
+  
   belongs_to :user
-
+  
   # Gets all the relationships where you are the parent
   has_many :relationships_as_a_parent, class_name: 'ArgumentParentChildRelationship', foreign_key: :parent_id
-
+  
   # Gets all the relationships where you are the child
   has_many :relationships_as_a_child, class_name: 'ArgumentParentChildRelationship', foreign_key: :child_id
-
+  
   # Taking all the relationships where you are the child, getting the parent and calling it parents
   has_many :parents, through: :relationships_as_a_child, source: :parent
   # Taking all the relationships where you are the child, getting the parent and calling it children
   has_many :children, through: :relationships_as_a_parent, source: :child
-
+  
   has_many :tags, through: :tags_argument
   has_many :notifications, dependent: :destroy
   has_many :votes, dependent: :destroy
-
+  
   validates :content, presence: true
 end
