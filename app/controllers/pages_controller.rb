2d6ac4816d91a4c850f67900fdd3221c7b2d0cfe
Nawel Aammar <nawel.aa@hotmail.fr>
2020-11-26 14:24:00 -0500

    Search bar working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,28 @@ class PagesController < ApplicationController
   end
 
   def search
-    @arguments = policy_scope(Argument).order(created_at: :desc)
+    if params[:search_form].nil? || params[:search_form][:results] == ""
+      @arguments = Argument.all.order(created_at: :desc)
+    elsif params[:search_form][:results] == "[]"
+      return
+    else
+      parse_results
+      
+      # FILTERS
+      # For each topic, search for the matches with @arguments
+      # Send this back to the view
+      # Take into account the tag param for displaying the results
+    end
+  end
+
+  private
+
+  def parse_results
+    arguments_ids = JSON.parse(params[:search_form][:results])
+
+    @arguments =
+      arguments_ids
+      .map { |id| Argument.find(id) }
+      .sort! { |a, b| a.created_at <=> b.created_at }
   end
 end
