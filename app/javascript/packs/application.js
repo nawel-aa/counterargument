740114d411f2c81f2e0ec139708688e925e01698
Tatiana Labrousse <mvtatianalabrousse@gmail.com>
2020-12-01 14:15:51 -0500

    modified socialshare gem for new asset pipeline

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require ("social-share-button")
+
+//=require social-share-button
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -24,6 +25,7 @@ require ("social-share-button")
 
 // External imports
 import "bootstrap";
+import "../plugins/social_share";
 
 import { initSidenavOnClick } from '../components/sidenav.js';
 import { initExpandableSearch } from '../components/expandable_search.js';
