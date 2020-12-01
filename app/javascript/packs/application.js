28ff16573c785630ae090f56a5a1a177bfc8e619
Tatiana Labrousse <mvtatianalabrousse@gmail.com>
2020-12-01 12:24:22 -0500

    added social share gem +visible on arg cards

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require ("social-share-button")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -42,7 +43,7 @@ import { initSelect2 } from "../plugins/init_select2";
 
 document.addEventListener('turbolinks:load', () => {
   initSelect2();
-  
+
   initSidenavOnClick();
   initExpandableSearch();
   initNotificationsCable();
