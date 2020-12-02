507e9779b2dea46ca6a3b6ee6de1eb76efd3db26
Patrick Cronin <pxcronin@gmail.com>
2020-12-02 12:00:01 -0500

    Home search working and stylized

--- app/javascript/components/expandable_search.js
+++ app/javascript/components/expandable_search.js
@@ -2,26 +2,26 @@ const initExpandableSearch = () => {
 const openSearch = document.querySelector('.iconsearch');
 const inputBar = document.querySelector('.search-input');
 const srchBtn = document.querySelector('.iconsearch2');
-
+  if (openSearch) {
 // Add listener to open search from click & render search icon invisible
  // && search btn visible
-  openSearch.addEventListener('click',() =>{
-    openSearch.classList.toggle('remove');
-    inputBar.classList.toggle('active');
-    srchBtn.classList.toggle('visible');
+    openSearch.addEventListener('click',() =>{
+      openSearch.classList.toggle('remove');
+      inputBar.classList.toggle('active');
+      srchBtn.classList.toggle('visible');
 
-  })
-// Reset when clicking anywhere except input bar & visible search btn
-  document.addEventListener('click',() =>{
-    let isClickOnSearch = openSearch.contains(event.target);
-    let isClickOnInput = inputBar.contains(event.target);
-    if (!isClickOnSearch && !isClickOnInput){
-    inputBar.classList.remove('active');
-    srchBtn.classList.remove('visible');
-    openSearch.classList.remove('remove');
-  }
-  })
-
+    })
+  // Reset when clicking anywhere except input bar & visible search btn
+    document.addEventListener('click',() =>{
+      let isClickOnSearch = openSearch.contains(event.target);
+      let isClickOnInput = inputBar.contains(event.target);
+      if (!isClickOnSearch && !isClickOnInput){
+      inputBar.classList.remove('active');
+      srchBtn.classList.remove('visible');
+      openSearch.classList.remove('remove');
+    }
+    })
+  }   
 }
 
 export { initExpandableSearch };
